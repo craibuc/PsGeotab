9047cb2adafcd337a9344e3507d2a164b35f1436
Craig Buchanan <craig.buchanan@cogniza.com>
2020-01-19 11:08:27 -0600

    initial commit

---
+++ Public/Get-DiagnosticType.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+List of DiagnosticTypes.
+
+.DESCRIPTION
+List of DiagnosticTypes.
+
+.INPUTS
+None.
+
+.OUTPUTS
+System.Management.Automation.PsCustomObject[].  Array of devices.
+
+.EXAMPLE
+Get all devices.
+PS> Get-DiagnosticType
+
+.LINK
+https://geotab.github.io/sdk/software/api/reference/#T:Geotab.Checkmate.ObjectModel.DeviceType
+
+#>
+function Get-DiagnosticType {
+
+    $csv = `
+@"
+Name,Description
+DataDiagnostic,Data Diagnostic
+GoDiagnostic,Go Diagnostic
+GoFault,Go Device Fault
+LegacyFault,Legacy Proprietary Fault
+None,No diagnostic
+ObdFault,OBD (Onboard Diagnostic) Fault
+ObdWwhFault,OBD (Onboard Diagnostic) WWH Fault
+Pid,PID (Parameter Identifier)
+ProprietaryFault,Proprietary Fault
+Sid,SID (Subsystem Identifier)
+SuspectParameter,SPN (Suspect Parameter Number)
+"@
+
+    $csv | ConvertFrom-Csv
+    
+}
