a2b56256dbb3370b5a7643e4bf438fbb778fd167
Craig Buchanan <craig.buchanan@cogniza.com>
2020-11-04 07:35:06 -0600

    initial commit

---
+++ PsGeotab/Public/Remove-GeotabEntity.ps1
@@ -0,0 +1,46 @@
+function Remove-GeotabEntity {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+		[Parameter(Mandatory)]
+		[PsCustomObject]$Session,
+
+        [Parameter(Mandatory)]
+		[string]$typeName,
+
+        [Parameter(Mandatory)]
+        [object]$entity
+    )
+    
+    begin {
+        $Uri = "https://$($Session.path)/apiv1"
+        Write-Debug "Uri: $Uri"
+    }
+    
+    process {
+        # payload        
+        $Body = @{
+            method = 'Remove'
+            params = @{ 
+                credentials = $Session.credentials
+                typeName = $typeName
+                entity = $entity
+            }
+        }
+        
+        # payload as JSON
+        Write-Debug ($Body | ConvertTo-Json -Depth 4)
+    
+        # POST
+        $Content = ( Invoke-WebRequest -Uri $uri -Method Post -Body ($Body | ConvertTo-Json -Depth 4) -ContentType "application/json" ).Content | ConvertFrom-Json
+    
+        # # returns PsCustomObject representation of object
+        if ( $Content.result ) { $Content.result }
+        # # otherwise raise an exception
+        elseif ($Content.error) { Write-Error -Message $Content.error.message }
+
+    }
+    
+    end {}
+
+}
---
+++ Tests/Public/Remove-GeotabEntity.Tests.ps1
@@ -0,0 +1,129 @@
+BeforeAll {
+
+    $ProjectDirectory = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
+
+    $PublicPath = Join-Path $ProjectDirectory "/PsGeotab/Public/"
+
+    $FixturesDirectory = Join-Path $ProjectDirectory "/Tests/Fixtures/"
+
+    $SUT = (Split-Path -Leaf $PSCommandPath) -replace '\.Tests\.', '.'
+
+    $Path = Join-Path $PublicPath $SUT
+    Write-Host "Path: $Path"
+
+    . (Join-Path $PublicPath $SUT)
+
+}
+
+Describe "Remove-GeotabEntity" -Tag 'unit' {
+
+    Context "Parameter validation" {
+
+        BeforeAll {
+            $Command = Get-Command 'Remove-GeotabEntity'
+        }
+
+        Context 'Session' {
+            BeforeAll {
+                $ParameterName = 'Session'
+            }
+
+            It "is a [PsCustomObject]" {
+                $Command | Should -HaveParameter $ParameterName -Type PsCustomObject
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+        Context 'typeName' {
+            BeforeAll {
+                $ParameterName = 'typeName'
+            }
+
+            It "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+        Context 'entity' {
+            BeforeAll {
+                $ParameterName = 'entity'
+            }
+
+            It "is a [object]" {
+                $Command | Should -HaveParameter $ParameterName -Type object
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+    } # /Content
+
+    Context "when Session, typeName, and entity parameters are supplied" {
+
+        BeforeAll {
+            # arrange
+            $Session = [PsCustomObject]@{
+                path = 'servername'
+                credentials = [PsCustomObject]@{
+                    sessionId = 123456
+                }
+            }
+            $typeName = 'User'
+            $entity = @{
+                id = 'b9'
+                firstName = 'First'
+                lastName = 'Last'
+            }
+
+            Mock Invoke-WebRequest {
+                $Fixture = 'Remove-User.Response.json'
+                $Content = Get-Content (Join-Path $FixturesDirectory $Fixture) -Raw
+
+                $Response = New-MockObject -Type  Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject
+                $Response | Add-Member -Type NoteProperty -Name 'Content' -Value $Content -Force
+                $Response
+            }
+        }
+
+        BeforeEach {
+            Remove-GeotabEntity -Session $Session -typeName $typeName -entity $entity
+        }
+
+        It "creates a POST request" {
+            Should -Invoke Invoke-WebRequest -ParameterFilter {
+                $Method -eq 'Post'
+            }
+        }
+
+        It "to the correct Uri" {
+            Should -Invoke Invoke-WebRequest -ParameterFilter {
+                $Uri -eq "https://$($Session.path)/apiv1"
+            }
+        }
+
+        It "using a ContentType of 'application/json'" {
+            Should -Invoke Invoke-WebRequest -ParameterFilter {
+                $ContentType -eq 'application/json'
+            }
+        }
+
+        It "with the desired Body payload" {
+            # assert
+            Should -Invoke Invoke-WebRequest -ParameterFilter {
+                $ParsedBody = ($Body | ConvertFrom-Json)
+                $ParsedBody.method -eq 'Remove' -and
+                $ParsedBody.params.typeName -eq $typeName -and
+                $ParsedBody.params.entity.id -ne $null -and
+                ( Compare-Object -ReferenceObject ([pscustomobject]$Entity) -DifferenceObject $ParsedBody.params.entity ).SideIndicator.Count -eq 0
+            }
+        }
+
+    }
+
+}
